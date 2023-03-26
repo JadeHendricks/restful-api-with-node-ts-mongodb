b1c95d9fd248068f8f738cfb6c53d36f4f249028
Jade <hendricksjade@gmail.com>
2023-03-26 16:08:15 +0200

    init(helpers): adding in helpers to for the authentication process and adding in a comment to users.ts

--- src/db/users.ts
+++ src/db/users.ts
@@ -35,6 +35,7 @@ export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne
     'authentication.sessionToken': sessionToken
 });
 export const getUserById = (id: string) => UserModel.findById(id);
+//toObject here is converting what comes from the DB to JS
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
---
+++ src/helpers/index.ts
@@ -0,0 +1,8 @@
+//encrypt the password or create a random token
+import crypto from "crypto";
+const dotenv = require("dotenv").config();
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac("sha256", [salt, password].join("/")).update(process.env.SECRET).digest("hex");
+}
