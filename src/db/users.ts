0098a57e4e8e4b589112b1082574588e0840fed3
Jade <hendricksjade@gmail.com>
2023-03-26 15:54:21 +0200

    init(schema/model/controller): adding in a user schema/model and adding in controller methods to query and update the DB.

---
+++ src/db/users.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+//select false means, that when we ask for this data via the api, the data will no go with the payload if false.
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    authentication: {
+        password: {
+            type: String,
+            required: true,
+            select: false
+        },
+        salt: {
+            type: String,
+            select: false
+        },
+        sessionToken: {
+            type: String,
+            select: false
+        }
+    }
+});
+
+export const UserModel = mongoose.model("User", userSchema);
+
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken': sessionToken
+});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
